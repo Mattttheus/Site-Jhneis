2890d185b911694a4effb5a33ec9860f542b02b1
Matheus Vinicius <matheusviniciuscaieiras@gmail.com>
2023-08-11 09:26:05 -0300

    Site Jhenis

---
+++ site.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="siitle.css">
+
+    <title>Bootstrap Example</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body class="menu">
+    <div class="btn-group">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"
+            _msttexthash="214071" _msthash="499"> Menu </button>
+        <ul class="dropdown-menu" _msthidden="3">
+            <li _msthidden="1"><a class="dropdown-item" href="#" _msttexthash="115609" _msthidden="1"
+                    _msthash="500">Contato</a></li>
+            <li _msthidden="1"><a class="dropdown-item" href="#" _msttexthash="115609" _msthidden="1"
+                    _msthash="501">Agenda De Eventos</a></li>
+            <li _msthidden="1"><a class="dropdown-item" href="#" _msttexthash="115609" _msthidden="1"
+                    _msthash="502">Equipe Pedagogica</a></li>
+        </ul>
+    </div>
+
+    <div class="btn-group">
+        <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"
+            _msttexthash="478647" _msthash="503"> informações </button>
+        <ul class="dropdown-menu dropdown-menu-end" style="" _mstvisible="0">
+            <li _mstvisible="1"><a class="dropdown-item" href="#" _msttexthash="157339" _msthash="504"
+                    _mstvisible="2">Quem Somos</a></li>
+            <li _mstvisible="1"><a class="dropdown-item" href="#" _msttexthash="157339" _msthash="505"
+                    _mstvisible="2">Processo Seletivo</a></li>
+            <li _mstvisible="1"><a class="dropdown-item" href="#" _msttexthash="157339" _msthash="506"
+                    _mstvisible="2">Mentorias</a></li>
+        </ul>
+    </div>
+
+    <div class="logo">
+        <img src="ima06.jpg">
+    </div>
+
+</body>
+
+
+</html>
